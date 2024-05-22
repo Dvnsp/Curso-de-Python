7569b1fab9b7e06a516b17ea3713fb1173d29f04
Dvnsp <duvanlv20@hotmail.com>
2024-05-22 12:32:52 +0200

    1.0

--- prueba.py
+++ prueba.py
@@ -1,14 +1,30 @@
-edad=23
-edad2=45
-edad2introducidad 
-__annotations__
-_loging= usuario
-_pasword= ususario
-suma= (8+3)
-print 
+import random
+def juego_adivinanza():
+    # generar un numero aleatorio entre 1 y 100
+    numero_secreto = random.randint(1,100)
+intentos = 0
+adivinado = False
 
-fn_edad=98 
-print(""
+print("¡bienvenido al juego de adivinanza!")
+print("he elegido un numero entre 1 y 100. ¿puedes adivinar cual es?")
+# bucle principal del juego
+while not adivinado:
+    try:
+        #pedir al usuario que introduzca un numero 
+        adivinanza = 
+        int(input("34 :"))
+        
+        intentos += 1
+        #comprobar si la adivinanza es correcta 
+        if adivinanza < nummero_secreto: 
+            print("demasiado bajo.intentalo de nuevo ")
+            elif adivinanza > numero_secreto
+            print("demasiado alto. intentalo de nuevo")
+            else: adivinado = True
+            print("felicidades has adivinado el numero en ¨{intentos}intentos")
 
-"ESTO ES UNA PRUEBA""")
-
+except ValueError: 
+    print("por favor, introduce un numero valido
+    #ejecutar el juego") 
+    if __name__ == "__main__": juego adivinanza 
+    
