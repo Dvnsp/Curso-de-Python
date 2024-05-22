5c5e36e1c2897f7681ab6ed6757eb171f9f38a38
Dvnsp <duvanlv20@hotmail.com>
2024-05-22 11:57:59 +0200

    vs3.0

--- prueba.py
+++ prueba.py
@@ -1,1 +1,14 @@
-print("hola mundo")
+edad=23
+edad2=45
+edad2introducidad 
+__annotations__
+_loging= usuario
+_pasword= ususario
+suma= (8+3)
+print 
+
+fn_edad=98 
+print(""
+
+"ESTO ES UNA PRUEBA""")
+
